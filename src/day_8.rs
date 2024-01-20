10c3c4270f5a86bc51d2636813390dfb3848af35
Topher Cawlfield <4094385+tcawlfield@users.noreply.github.com>
2024-01-20 00:40:36 -0700

    Day 8 pt 2 NOT OK
    Needs time optimization to find loops in individual sequences.

--- src/day_8.rs
+++ src/day_8.rs
@@ -10,6 +10,9 @@ pub fn main() {
     let docs = Docs::from_file(utils::lines_in_file(INPUT)).unwrap();
     let steps = docs.steps_to_zzz();
     println!("Reaching ZZZ in {steps} steps");
+
+    let aa_steps = docs.steps_from_all_a_to_any_z();
+    println!("Going from ??A to ??Z in {aa_steps} steps");
 }
 
 struct Docs {
@@ -25,8 +28,7 @@ impl Docs {
         I::Item: Borrow<str>,
     {
         lazy_static! {
-            static ref MAP_RE: Regex =
-                Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+            static ref MAP_RE: Regex = Regex::new(r"(\w{3}) = \((\w{3}), (\w{3})\)").unwrap();
         }
         let mut lines = lines.into_iter();
         let instructions = parse_instructions(lines.next()?.borrow());
@@ -58,6 +60,34 @@ impl Docs {
             loc = &paths[*direction as usize][..];
             steps += 1;
             if loc == end {
+                break;
+            }
+        }
+        steps
+    }
+
+    fn steps_from_all_a_to_any_z(&self) -> i64 {
+        let mut steps = 0;
+        let mut locs: Vec<&String> = self.map.keys().filter(|loc| loc.ends_with("A")).collect();
+        let mut instructions = Box::new(self.instructions.iter());
+        println!("Locs: {locs:?}");
+        loop {
+            let direction = if let Some(i) = instructions.next() {
+                i
+            } else {
+                *instructions = self.instructions.iter(); // go back through
+                instructions.next().unwrap() // there had better be a first direction to go
+            };
+            for loc in locs.iter_mut() {
+                let paths = self.map.get(*loc).unwrap();
+                *loc = &paths[*direction as usize];
+            }
+            steps += 1;
+            if steps % 1000000 == 0 {
+                let terminal_locs = locs.iter().filter(|loc| loc.ends_with("Z")).count();
+                println!("Step {steps}, Locs: {locs:?}, {terminal_locs} terminal.");
+            }
+            if locs.iter().all(|loc| loc.ends_with("Z")) {
                 break;
             }
         }
@@ -96,13 +126,30 @@ mod tests {
     BBB = (AAA, ZZZ)
     ZZZ = (ZZZ, ZZZ)"#;
 
+    const EXAMPLE_3: &str = r#"LR
+
+    11A = (11B, XXX)
+    11B = (XXX, 11Z)
+    11Z = (11B, XXX)
+    22A = (22B, XXX)
+    22B = (22C, 22C)
+    22C = (22Z, 22Z)
+    22Z = (22B, 22B)
+    XXX = (XXX, XXX)"#;
+
     #[test]
-    fn test_parse_example() {
+    fn test_part1_examples() {
         let docs = Docs::from_file(EXAMPLE_1.lines()).unwrap();
         assert_eq!(docs.map.len(), 7);
         assert_eq!(docs.steps_to_zzz(), 2);
 
         let docs2 = Docs::from_file(EXAMPLE_2.lines()).unwrap();
         assert_eq!(docs2.steps_to_zzz(), 6);
+    }
+
+    #[test]
+    fn test_pt2_example() {
+        let docs = Docs::from_file(EXAMPLE_3.lines()).unwrap();
+        assert_eq!(docs.steps_from_all_a_to_any_z(), 6);
     }
 }
