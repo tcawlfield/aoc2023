08e1e73d852b6e5f907059411fb88b1edfd04c15
Topher Cawlfield <4094385+tcawlfield@users.noreply.github.com>
2024-01-24 02:35:41 -0700

    Better C++ style

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,18 +4,15 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        cout << "Specify a day number" << endl;
-        return 1;
-    }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << "Specify a day number" << endl;
+    return 1;
+  }
 
-    int day = strtol(argv[1], NULL, 10);
-    if (day == 9)
-    {
-        return day9::main();
-    }
-    return 2;
+  int day = strtol(argv[1], NULL, 10);
+  if (day == 9) {
+    return day9::main();
+  }
+  return 2;
 }
